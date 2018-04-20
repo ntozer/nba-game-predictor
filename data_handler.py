10046f1bdab66f8b1fbebd946718d1dafdafcc53
Nathan Tozer <ntozer@unb.ca>
2018-04-19 22:57:53 -0400

    season to season ELO soft reset update

--- data_handler.py
+++ data_handler.py
@@ -2,7 +2,41 @@ import pandas as pd
 import numpy as np
 
 
+def load_raw_data(seasons, months=['october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june']):
+    """
+    Params:
+        seasons - the seasons to load data from
+        months - the months to load data from
+    Returns:
+        df_list - list containing a dataframe for each season loaded
+    """
+    df_list = []
+    for season in seasons:
+        print('Loading raw data from /data/raw/NBA_{s}/[months].csv'.format(s=season))
+        df = None
+        for month in months:
+            try:
+                temp_df = pd.read_csv('data/raw/NBA_{s}/games-{m}.csv'.format(s=season, m=month))
+                if df is None:
+                    df = temp_df
+                else:
+                    df = df.append(temp_df, ignore_index=True)
+            except FileNotFoundError:
+                print('No data for month of', month)
+                continue
+                
+        df_list.append(df)
+    
+    return df_list
+
+
 def load_prep_data(seasons):
+    """
+    Params:
+        seasons - the seasons from which to load data
+    Returns:
+        df - dataframe containing the seasons games
+    """
     df = None
     for season in seasons:
         try:
